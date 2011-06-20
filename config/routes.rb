6f81c0c73bb0e9302e7aacf417624356f69fa9b7
John Bain <bain@linux-on2w.site>
2011-06-19 20:19:16 -0400

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 SampleApp::Application.routes.draw do
 
 
+  get "users/new"
+
   get "pages/contract"
 
   get "pages/about"
   
   get "pages/help"
   
+  match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contract'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
